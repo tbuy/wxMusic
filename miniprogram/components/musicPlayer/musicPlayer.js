cd3dd10c2e33feaeea408425993f60f51cbb0adc
wangqiuying <wangqiuying@ihmair.cn>
2019-12-17 17:08:27 +0800

    no message

--- miniprogram/components/musicPlayer/musicPlayer.js
+++ miniprogram/components/musicPlayer/musicPlayer.js
@@ -14,24 +14,43 @@ Component({
    * 组件的初始数据
    */
   data: {
-    musicTime:{
-      startTime:'0:00',
-      endTime:'0:00'
-    }
-  },
+    musicTime: {
+      startTime: '0:00',
+      endTime: '0:00'
+    },
+    movableDis: 0,
+    progress: 0,
 
+
+  },
+  movableAreaVal: 0,
+  movableViewVal: 0,
+  _getMovableDis() {
+    const query = wx.createSelectorQuery();
+    query.select('.movable-area').boundingClientRect();
+    query.select('.movable-view').boundingClientRect();
+    query.exec(val => {
+      this.movableAreaVal = val[0].width;
+      this.movableViewVal = val[1].width;
+      console.log(val)
+    })
+
+  },
+  changeMovable() {
+    this._getMovableDis()
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-    initMusic(){
+    initMusic() {
       backgroundAudioManager.title = '此时此刻'
       backgroundAudioManager.epname = '此时此刻'
       backgroundAudioManager.singer = '许巍'
       backgroundAudioManager.dataUrl = 'cloud://free1.6672-free1-1300046898/music/music1.mp3'
       backgroundAudioManager.coverImgUrl = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
     },
-    playMusic(){
+    playMusic() {
       backgroundAudioManager.play()
     },
     pauseMusic() {
--- miniprogram/pages/comment/comment.js
+++ miniprogram/pages/comment/comment.js
@@ -1,5 +1,6 @@
-// pages/playmusic/playmusic.js
+// pages/comment/comment.js
 Page({
+
   /**
    * 页面的初始数据
    */
@@ -11,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this._getMovableDis();   
+
   },
 
   /**
